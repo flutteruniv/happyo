5ef6bcd7465a6d7b016e1faea1f563a7bf46f476
momma.k <c011533617@edu.teu.ac.jp>
2022-08-13 10:32:25 +0900

    不要なコードを削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:happyo/testModel.dart';
-import 'package:provider/provider.dart';
 
 import 'widgets/custom_tab_bar.dart';
 
@@ -27,11 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChangeNotifierProvider<testModel>(
-        // createでfetchBooks()も呼び出すようにしておく。
-        create: (_) => testModel()..fetchBooks(),
-        child: const testUI(),
-      )
+      home: const MyHomePage(),
     );
   }
 }
@@ -68,37 +62,6 @@ class MyHomePage extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class testUI extends StatelessWidget {
-  const testUI({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('一覧'),
-      ),
-      body: Consumer<testModel>(
-          builder: (context, model, child) {
-            // FirestoreのドキュメントのList testsを取り出す。
-            final tests = model.tests;
-            return ListView.builder(
-              // Listの長さを先ほど取り出したtestsの長さにする。
-              itemCount: tests.length,
-              // indexにはListのindexが入る。
-              itemBuilder: (context, index) {
-                return ListTile(
-                  //　books[index]でList booksのindex番目の要素が取り出せる。
-                  title: Text(tests[index].test_field),
-                  subtitle: Text(tests[index].created_at.toDate().toString()),
-                );
-              },
-            );
-          },
-      )
     );
   }
 }
