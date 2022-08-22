58ecf57e980d8d0465ea82463cba2927304d0463
m-tanaka <m-tanaka@kiban-lab.com>
2022-08-23 08:39:34 +0900

    feat: 画面遷移に関する処理を定義

---
+++ lib/common/routes.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const String index = '/';
+
+  static void pushNamed(BuildContext context, String routeName,
+      {Object? args}) {
+    Navigator.pushNamed(context, routeName, arguments: args);
+  }
+
+  static void pop(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  static dynamic getArgs(BuildContext context) {
+    return ModalRoute.of(context)!.settings.arguments;
+  }
+}
