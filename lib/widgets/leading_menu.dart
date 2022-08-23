e66cd0f68c16d925d7f09593a08045262731489f
m-tanaka <m-tanaka@kiban-lab.com>
2022-08-24 00:49:45 +0900

    feat: メニューボタンを部品化

---
+++ lib/widgets/leading_menu.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LeadingMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.menu),
+    );
+  }
+}
