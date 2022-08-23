d08c2929264d622929e269f75b511bf6dbd3f7c1
m-tanaka <m-tanaka@kiban-lab.com>
2022-08-24 00:49:26 +0900

    feat: タイトルロゴを部品化

---
+++ lib/widgets/title_logo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TitleLogo extends StatelessWidget {
+  const TitleLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('HAPPYO');
+  }
+}
