17a2ec9510e0a179f8fe4d49ca4a76de6ea312bb
momma.k <c011533617@edu.teu.ac.jp>
2022-09-10 19:32:54 +0900

    コンフリクト解消

--- lib/page/profile/profile_page.dart
+++ lib/page/profile/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
@@ -61,6 +63,25 @@ class UserProfileHeader extends StatelessWidget {
 }
 
 class GuestProfileHeader extends StatelessWidget {
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+    await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -79,7 +100,9 @@ class GuestProfileHeader extends StatelessWidget {
                 child: const Text('新規登録'),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  signInWithGoogle();
+                },
                 child: const Text('ログイン'),
               ),
             ],
