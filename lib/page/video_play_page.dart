2af6b769c850c79be293860fe969426861973b8c
m-tanaka <m-tanaka@kiban-lab.com>
2022-09-22 23:36:59 +0900

    動画再生画面のDrawerを削除

--- lib/page/video_play_page.dart
+++ lib/page/video_play_page.dart
@@ -70,7 +70,6 @@ class _VideoPlayPageState extends State<VideoPlayPage> {
           ),
         ],
       ),
-      drawer: SideMenu(),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
